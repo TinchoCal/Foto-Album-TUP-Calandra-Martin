825a4e279bcb99139262969596ef5a6da24aef31
TinchoCal <tinchocal06@gmail.com>
2024-11-09 14:37:45 -0300

    first commit

---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Photo {self.title}>'
---
+++ routes.py
@@ -0,0 +1,21 @@
+from flask import render_template, request, redirect, url_for, flash
+from app import app, db
+from models import Photo
+from forms import PhotoForm
+import base64
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_photo():
+    form = PhotoForm()
+    if form.validate_on_submit():
+        # Leer el archivo de imagen
+        image_file = form.image.data
+        image_data = base64.b64encode(image_file.read()).decode('utf-8')  # Convertir a base64
+
+        new_photo = Photo(title=form.title.data, description=form.description.data, image=image_data)
+        db.session.add(new_photo)
+        db.session.commit()
+        flash('Photo added successfully!', 'success')
+        return redirect(url_for('index'))
+    
+    return render_template('photo_form.html', form=form)
